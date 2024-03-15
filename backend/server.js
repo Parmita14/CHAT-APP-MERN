7ffc54066109cca08f9137bb71e4dde10213adfd
Parmita14 <parmitaupadhyay3@gmail.com>
2024-03-15 20:17:58 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoute");
+const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 connectDB();
